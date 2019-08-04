9fd1687a1c3a4adec8bcf1f7cb0f67f2adae75a2
luaquecaiu3 <luaquecaiu3@users.noreply.github.com>
2019-08-04 15:09:40 -0300

    a

---
+++ js/functions.js
@@ -0,0 +1,39 @@
+
+// Smooth Scrolling
+//==========================================
+$(function() {
+  $('a.scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({scrollTop: target.offset().top -50}, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// force show placeholder
+$(function(){
+	$('input, textarea').placeholder();
+});
+
+//my functions
+$(function(){
+  var x;
+  var y = null;
+  var z = 5;
+
+  var w = 'hello';
+  
+document.getElementById("content").innerHTML = "Hello World";​
+/*
+  document.querySelector('.content').innerHTML = 'x: ' + x + ' typeof: ' + typeof x
+  + '\ny: ' + y + ' typeof: ' + typeof y
+  + '\nz: ' + z + ' typeof: ' + typeof z
+  + '\nw: ' + w + ' typeof: ' + typeof w
+;*/
+
+
+});
